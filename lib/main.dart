91aee6e370fdb24bd45ff76994241b970a018297
Maurice Gerhardt <maurice@mgcodes.one>
2020-02-04 11:40:51 +0100

    renamed recording_page to rec_page & made it a stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:eloquent_hackathon/screens/recording_page.dart';
+import 'package:eloquent_hackathon/screens/rec_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: RecordingPage(),
+      home: RecPage(),
       debugShowCheckedModeBanner: false,
     );
   }
--- lib/screens/rec_page.dart
+++ lib/screens/rec_page.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 class RecPage extends StatefulWidget {
@@ -7,12 +8,40 @@ class RecPage extends StatefulWidget {
 
 class _RecPageState extends State<RecPage> {
 
-  
+  double _value = 0.0;
+  bool _isRecording = false;
+
+  void _recordSpeech(){
+    new Timer.periodic(new Duration(milliseconds: 10), (timer) {
+      setState((){
+        if (!_isRecording){
+          timer.cancel();
+          return;
+        }
+        _value += 0.01;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ELOQUENT'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 50),
+            Text("Record your speech to get feedback on it"),
+            SizedBox(height: 20),
+            Text(_value.toString()),
+            SizedBox(height: 10),
+            IconButton(icon: Icon(Icons.mic_none), onPressed: _recordSpeech)
+
+          ],
+        )
+      )
+      );
   }
 }
