e4f64f37c3e30eef93b18835bef12b664082d0b2
Maurice Gerhardt <maurice@mgcodes.one>
2020-02-04 15:05:05 +0100

    this branch doesnt matter anymore

--- lib/screens/rec_page.dart
+++ lib/screens/rec_page.dart
@@ -1,5 +1,9 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_audio_recorder/flutter_audio_recorder.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class RecPage extends StatefulWidget {
   @override
@@ -8,36 +12,66 @@ class RecPage extends StatefulWidget {
 
 class _RecPageState extends State<RecPage> {
 
+  FlutterAudioRecorder _recorder;
+  Recording _recording;
   int _hour = 0;
   int _min = 0;
   int _sec = 0;
   bool _isRecording = false;
+  String _customPath = '/eloquent_hackathon';
 
-  void _recordSpeech(){
-    if (_isRecording) {
-      _isRecording = false;  
-    } else {
-      _isRecording = true;
-    }
-  
-    new Timer.periodic(new Duration(milliseconds: 1000), (timer) {
-      setState((){
-        if (_isRecording == false){
-          timer.cancel();
-          return;
+  Future<void> _recordSpeech() async {
+    bool hasPermission = await FlutterAudioRecorder.hasPermissions;
+
+    if (hasPermission) {
+    Directory appDocDirectory;
+        if (Platform.isIOS) {
+          appDocDirectory = await getApplicationDocumentsDirectory();
+        } else {
+          appDocDirectory = await getExternalStorageDirectory();
         }
-        _sec += 1;
-        if (_sec > 59) {
-          _sec = 0;
-          _min += 1;
-          if (_min > 59 && _sec > 59) {
-            _sec = 0;
-            _min = 0;
-            _hour += 1;
+
+        _customPath = appDocDirectory.path +
+          _customPath +
+          DateTime.now().millisecondsSinceEpoch.toString();
+        print("customPath: $_customPath");
+      if (_isRecording) {
+        _isRecording = false;
+        
+        var result = await _recorder.stop();
+        setState(() {
+          _recording = result;
+        });
+        AudioPlayer player = AudioPlayer();
+        File file = new File(_recording.path);
+        player.play(file.path, isLocal: true);
+      
+      } else {
+        _isRecording = true;
+        _recorder = FlutterAudioRecorder(_customPath, audioFormat: AudioFormat.AAC);
+        await _recorder.initialized;
+        await _recorder.start();
+        _recording = await _recorder.current(channel: 0);
+      }
+      new Timer.periodic(new Duration(milliseconds: 1000), (timer) {
+        setState((){
+          if (_isRecording == false){
+            timer.cancel();
+              return;
+            }
+            _sec += 1;
+            if (_sec > 59) {
+              _sec = 0;
+              _min += 1;
+              if (_min > 59 && _sec > 59) {
+                _sec = 0;
+                _min = 0;
+                _hour += 1;
+            }
           }
-        }
+        });
       });
-    });
+    }
   }
 
   @override
