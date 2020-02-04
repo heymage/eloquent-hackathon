eed97ddf052dd24cde0245893475c671f1935ea6
Maurice Gerhardt <maurice@mgcodes.one>
2020-02-04 11:34:25 +0100

    start with basic UI

---
+++ lib/screens/rec_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class RecPage extends StatefulWidget {
+  @override
+  _RecPageState createState() => _RecPageState();
+}
+
+class _RecPageState extends State<RecPage> {
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
--- lib/screens/recording_page.dart
+++ lib/screens/recording_page.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 
 class RecordingPage extends StatelessWidget {
+
+  void recordSpeech() {
+    print("Recording...");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('ELOQUENT'),
       ),
-    );
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 50),
+            Text("Record your speech to get feedback on it"),
+            SizedBox(height: 20),
+            Text("00:00:00"),
+            SizedBox(height: 10),
+            IconButton(icon: Icon(Icons.mic_none), onPressed: recordSpeech)
+
+          ],
+        )
+      )
+      );
   }
 }
